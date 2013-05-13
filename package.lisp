bc1fc17f835c76be66decca338015ea922590753
g000001 <chiba.masaomi@gmail.com>
2013-05-14 04:06:32 +0900

    mod: formatting.

--- package.lisp
+++ package.lisp
@@ -2,6 +2,7 @@
 
 (cl:in-package :cl-user)
 
+
 (defpackage :tagger
   (:use)
   (:export))
@@ -34,6 +35,7 @@
   (:export #:make-ssb #:ssb-buffer #:ssb-pointer #:ssb-push-extend #:ssb-string
 	   #:copy-ssb #:reinitialize-ssb #:ssb-read-line #:ssb-string=))
 
+
 (cl:defpackage :string-resource
   (:use :common-lisp :cl-extensions)
   (:export #:+max-string-length+
@@ -46,25 +48,25 @@
   #+clos(:import-from clos slot-definition-name class-slots)
   #+pcl (:import-from pcl slot-definition-name class-slots)
   #+mcl (:import-from ccl slot-definition-name class-instance-slots)
-  (:export :ordered-set :set-order-fn :set-length :set-insert :set-clear
-	   :set-copy :set-union :set-intersect :set-minus :set-map
-	   :set-member-p :set-empty-p
+  (:export #:ordered-set #:set-order-fn #:set-length #:set-insert #:set-clear
+	   #:set-copy #:set-union #:set-intersect #:set-minus #:set-map
+	   #:set-member-p #:set-empty-p
 	   
-	   :relation :relation-get :relation-map :relation-empty-p
+	   #:relation #:relation-get #:relation-map #:relation-empty-p
 	   
-	   :fsa :nfa :dfa
-	   :*epsilon* :fsa-start-state :fsa-delta :fsa-final-states
+	   #:fsa #:nfa #:dfa
+	   #:*epsilon* #:fsa-start-state #:fsa-delta #:fsa-final-states
 	   
-	   :symbol-order-fn :state-order-fn
-	   :make-state :make-nfa :make-dfa :make-ordered-set :make-relation
-	   :make-state-map :copy-delta :make-state-relation
-	   :symbols-get :delta-get :states-get
+	   #:symbol-order-fn #:state-order-fn
+	   #:make-state #:make-nfa #:make-dfa #:make-ordered-set #:make-relation
+	   #:make-state-map #:copy-delta #:make-state-relation
+	   #:symbols-get #:delta-get #:states-get
 	   
-	   :in-language-p :determinize :invert :minimize )
+	   #:in-language-p #:determinize #:invert #:minimize )
   (:export #:fsa-concat #:fsa-closure #:fsa-plus #:fsa-union #:fsa-optional
-	    #:fsa-minus #:fsa-symbol
+           #:fsa-minus #:fsa-symbol
 
-	    #:regexp-to-fsa #:* #:+ #:? #:- #:/ #:or #:! #:sequence))
+           #:regexp-to-fsa #:* #:+ #:? #:- #:/ #:or #:! #:sequence))
 
 
 (cl:defpackage :sv-resource
@@ -118,31 +120,35 @@
   (:use :common-lisp :fsa)
   (:export #:fixnum-states #:last-state))
 
+
 (cl:defpackage :standard-symbols
   (:use :common-lisp :fsa)
   (:export #:char-symbols #:symbol-symbols #:fixnum-symbols #:standard-symbols)
   (:export #:transducer-symbols
 	   #:make-transducer-pair #:transducer-pair-p #:tp-upper #:tp-lower))
 
+
 (cl:defpackage :list-sets
   (:use :common-lisp :fsa)
-  (:export :list-sets :list-set))
+  (:export #:list-sets #:list-set))
+
 
 (cl:defpackage :skip-list
   (:use :cl)
-  (:export
-   ;; high-level interface
-   #:skip-list #:make-skip-list #:free-skip-list
-   #:skip-list-length #:skip-list-empty-p
-   #:skip-list-get #:skip-list-remove
-   #:skip-list-top #:skip-list-pop
-   #:skip-list-merge #:skip-list-copy
-   #:skip-list-union #:skip-list-intersection #:skip-list-difference
-   #:do-skip-list #:map-skip-list #:clear-skip-list))
+  ;; high-level interface
+  (:export #:skip-list #:make-skip-list #:free-skip-list
+           #:skip-list-length #:skip-list-empty-p
+           #:skip-list-get #:skip-list-remove
+           #:skip-list-top #:skip-list-pop
+           #:skip-list-merge #:skip-list-copy
+           #:skip-list-union #:skip-list-intersection #:skip-list-difference
+           #:do-skip-list #:map-skip-list #:clear-skip-list))
+
 
 (cl:defpackage :skip-list-relations
   (:use :common-lisp :fsa :skip-list)
-  (:export :skip-list-relations :skip-list-relation))
+  (:export #:skip-list-relations #:skip-list-relation))
+
 
 (cl:defpackage :fsa-standard
   (:use :common-lisp :fsa :standard-states :standard-symbols
@@ -175,6 +181,7 @@
 	   #:trie-node-key #:trie-node-value #:walk-string
 	   #:make-simple-vector-trie #:svt-get #:map-svt
 	   #:sort-trie))
+
 
 (cl:defpackage :tag-basics
   (:use :common-lisp :cl-extensions :trie :tdb :fsa-tokenizer)
@@ -235,19 +242,20 @@
   (:use :common-lisp :cl-extensions :variable-storage
         :tagger.internal) 
   ;;may want to add binary-io  later
-  (:export
-   #:resource-type #:fixvref #:adjust-lfixv #:fill-sv #:fixnum-vector #:fill-sfv
-   #:let-sfv #:clear-byte32v-storage #:sfv-length #:sfv #:sv #:alloc-lsv
-   #:adjust-byte32v #:make-byte32v #:sfv-add #:fill-lsfv #:fixv #:adjust-lsfv
-   #:alloc-lsfv #:adjust-lsv #:fill-fixv #:clear-fixv-storage #:byte32vref
-   #:fill-lfixv #:adjust-fixv #:alloc-sfv #:let-byte32v #:let-sv #:sfv-max
-   #:fill-lsv #:alloc-fixv #:fill-lbyte32v #:copy-byte32v #:free-byte32v #:lert*
-   #:free-fixv #:lert #:sfv-div #:copy-sv #:byte32-vector #:alloc-lfixv #:sfvref
-   #:make-sv #:copy-sfv #:let-fixv #:copy-fixv #:adjust-lbyte32v #:fixv-length
-   #:free-sfv #:adjust-sv #:adjust-sfv #:byte32v-length #:fill-byte32v
-   #:clear-sfv-storage #:clear-sv-storage #:make-fixv #:free-fn #:alloc-lbyte32v
-   #:alloc-byte32v #:free-sv #:make-sfv #:byte32v #:alloc-sv #:sv-length
-   #:single-float-vector)
+  (:export #:resource-type #:fixvref #:adjust-lfixv #:fill-sv
+           #:fixnum-vector #:fill-sfv #:let-sfv #:clear-byte32v-storage
+           #:sfv-length #:sfv #:sv #:alloc-lsv #:adjust-byte32v #:make-byte32v
+           #:sfv-add #:fill-lsfv #:fixv #:adjust-lsfv #:alloc-lsfv #:adjust-lsv
+           #:fill-fixv #:clear-fixv-storage #:byte32vref #:fill-lfixv
+           #:adjust-fixv #:alloc-sfv #:let-byte32v #:let-sv #:sfv-max
+           #:fill-lsv #:alloc-fixv #:fill-lbyte32v #:copy-byte32v
+           #:free-byte32v #:lert* #:free-fixv #:lert #:sfv-div #:copy-sv
+           #:byte32-vector #:alloc-lfixv #:sfvref #:make-sv #:copy-sfv
+           #:let-fixv #:copy-fixv #:adjust-lbyte32v #:fixv-length #:free-sfv
+           #:adjust-sv #:adjust-sfv #:byte32v-length #:fill-byte32v
+           #:clear-sfv-storage #:clear-sv-storage #:make-fixv #:free-fn
+           #:alloc-lbyte32v #:alloc-byte32v #:free-sv #:make-sfv #:byte32v
+           #:alloc-sv #:sv-length #:single-float-vector)
   (:export #:lert #:lert* #:free-fn #:resource-type
            #:let-sfv #:copy-sfv))
 
@@ -271,11 +279,13 @@
 	   #:hmm-read-file #:hmm-write-file #:hmm-read #:hmm-write
 	   #:make-sfm #:sfmref))
 
+
 (cl:defpackage :tag-analysis
   (:use :common-lisp :cl-extensions :cons-resource :string-resource :svb :ssb
 	:sv-resource :hmm :tdb :fsa-tokenizer :tag-basics)
   (:export #:tag-analysis #:tagging-ts #:token-class
 	   #:tag-file #:tag-string #:tokenize-file))
+
 
 (cl:defpackage :class-guesser
   (:use :common-lisp :cl-extensions :cons-resource :string-resource
@@ -284,11 +294,13 @@
   (:export #:write-string-trie #:read-string-trie
 	   #:train-guesser-on-files #:suffix-lexicon #:suffix-pathname))
 
+
 (cl:defpackage :vector-lexicon
   (:use :common-lisp :cl-extensions :string-resource :cons-resource
 	:sv-resource :svb :ssb :skip-list :binary-io :trie :tdb)
   (:export #:vector-lexicon #:read-vector-lexicon #:write-vector-lexicon)
   (:shadow #:index))
+
 
 (cl:defpackage :tag-brown
   (:use :common-lisp :cl-extensions :string-resource :ssb :trie :tdb
@@ -303,9 +315,13 @@
 	:tag-analysis :tag-brown)
   (:export #:english-tokenizer #:*english-tokenizer-fsa*))
 
+
 (cl:defpackage :tag-trainer
   (:use :common-lisp :cl-extensions :cons-resource :string-resource
 	:sv-resource :hmm :float-vector
 	:tdb :tag-basics)
   (:shadow #:pi)
   (:export #:train-on-docs #:train-on-files #:train-combination))
+
+
+;;; *EOF*
